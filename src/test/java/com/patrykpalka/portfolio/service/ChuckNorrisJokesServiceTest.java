871ad30eb6b311507ec4d0e921fc1e4998fbf1b4
patrykpalka <patryk-palka2@wp.pl>
2025-01-14 00:47:52 +0100

    Test: Add test for jokes service class

---
+++ src/test/java/com/patrykpalka/portfolio/service/ChuckNorrisJokesServiceTest.java
@@ -0,0 +1,64 @@
+package com.patrykpalka.portfolio.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ChuckNorrisJokesServiceTest {
+
+    @InjectMocks
+    private ChuckNorrisJokesService chuckNorrisJokesService;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Test
+    void shouldReturnJokeWhenApiIsAvailable() {
+        // given
+        Map<String, String> apiResponse = Map.of("value", "Chuck Norris can divide by zero.");
+        ResponseEntity<Map> responseEntity = ResponseEntity.ok(apiResponse);
+
+        when(restTemplate.exchange(
+                "https://api.chucknorris.io/jokes/random",
+                HttpMethod.GET,
+                null,
+                Map.class
+        )).thenReturn(responseEntity);
+
+        // when
+        String response = chuckNorrisJokesService.getJoke();
+
+        // then
+        Assertions.assertEquals("Chuck Norris can divide by zero.", response);
+    }
+
+    @Test
+    void shouldReturnNoJokeFoundWhenApiReturnsNull() {
+        // given
+        ResponseEntity<Map> responseEntity = ResponseEntity.ok(null);
+
+        when(restTemplate.exchange(
+                "https://api.chucknorris.io/jokes/random",
+                HttpMethod.GET,
+                null,
+                Map.class
+        )).thenReturn(responseEntity);
+
+        // when
+        String response = chuckNorrisJokesService.getJoke();
+
+        // then
+        Assertions.assertEquals("No joke found!", response);
+    }
+}
