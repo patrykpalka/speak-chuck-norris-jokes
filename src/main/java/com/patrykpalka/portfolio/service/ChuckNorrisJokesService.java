1e34bfcaf75416179e854d5f7978f69427e846f6
patrykpalka <patryk-palka2@wp.pl>
2025-01-13 23:14:39 +0100

    Feat: Add service connecting external Chuck Norris API

---
+++ src/main/java/com/patrykpalka/portfolio/service/ChuckNorrisJokesService.java
@@ -0,0 +1,27 @@
+package com.patrykpalka.portfolio.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class ChuckNorrisJokesService {
+
+    private final RestTemplate restTemplate;
+    private final String apiUrl = "https://api.chucknorris.io/jokes/random";
+
+    @Autowired
+    public ChuckNorrisJokesService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getJoke() {
+        ResponseEntity<Map> response = restTemplate.exchange(apiUrl, HttpMethod.GET, null, Map.class);
+        Map<String, String> responseBody = response.getBody();
+        return responseBody != null ? responseBody.get("value") : "No joke found!";
+    }
+}
