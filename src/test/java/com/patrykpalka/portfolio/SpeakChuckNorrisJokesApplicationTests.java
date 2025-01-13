87084c2f4800d918ce4c7e659a50040855476c06
patrykpalka <patryk-palka2@wp.pl>
2025-01-13 22:35:57 +0100

    Initial commit

---
+++ src/test/java/com/patrykpalka/portfolio/SpeakChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrykpalka.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakChuckNorrisJokesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
