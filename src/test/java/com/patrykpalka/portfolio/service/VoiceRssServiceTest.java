a3a910393e1ae89611aa13bf06cddb1ff3f07531
patrykpalka <patryk-palka2@wp.pl>
2025-01-18 14:40:13 +0100

    Test: Add additional tests for uncovered lines in VoiceRssService

--- src/test/java/com/patrykpalka/portfolio/service/VoiceRssServiceTest.java
+++ src/test/java/com/patrykpalka/portfolio/service/VoiceRssServiceTest.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,19 +21,19 @@ class VoiceRssServiceTest {
     @Mock
     private RestTemplate restTemplate;
 
+    String urlFormat = "https://api.voicerss.org/?key=%s&hl=%s&v=%s&c=%s&f=%s&src=%s";
+
+    String key = "279b6ea964d74e8aa6ccb53a08593545";
+    String language = "en-us";
+    String voice = "Mary";
+    String audioCodec = "WAV";
+    String audioFormat = "44khz_16bit_stereo";
+
+    String apiUrl = String.format(urlFormat, key, language, voice, audioCodec, audioFormat, "test");
+
     @Test
     void shouldReturnAudioDataWhenApiIsAvailable() {
         // given
-        String urlFormat = "https://api.voicerss.org/?key=%s&hl=%s&v=%s&c=%s&f=%s&src=%s";
-
-        String key = "279b6ea964d74e8aa6ccb53a08593545";
-        String language = "en-us";
-        String voice = "Mary";
-        String audioCodec = "WAV";
-        String audioFormat = "44khz_16bit_stereo";
-
-        String apiUrl = String.format(urlFormat, key, language, voice, audioCodec, audioFormat, "test");
-
         byte[] audioData = "audio".getBytes();
         ResponseEntity<byte[]> responseEntity = ResponseEntity.ok(audioData);
 
@@ -53,20 +52,45 @@ class VoiceRssServiceTest {
     @Test
     void shouldReturnNullWhenApiCallFails() {
         // given
-        String urlFormat = "https://api.voicerss.org/?key=%s&hl=%s&v=%s&c=%s&f=%s&src=%s";
+        when(restTemplate.getForEntity(
+                apiUrl,
+                byte[].class
+        )).thenThrow(new RestClientException("Invalid response from Voice RSS API"));
 
-        String key = "279b6ea964d74e8aa6ccb53a08593545";
-        String language = "en-us";
-        String voice = "Mary";
-        String audioCodec = "WAV";
-        String audioFormat = "44khz_16bit_stereo";
+        // when
+        byte[] response = voiceRssService.getVoiceRss("test");
 
-        String apiUrl = String.format(urlFormat, key, language, voice, audioCodec, audioFormat, "test");
+        // then
+        Assertions.assertNull(response);
+    }
+
+    @Test
+    void shouldReturnNullWhenResponseBodyIsNull() {
+        // given
+        ResponseEntity<byte[]> responseEntity = ResponseEntity.ok(null);
 
         when(restTemplate.getForEntity(
                 apiUrl,
                 byte[].class
-        )).thenThrow(new RestClientException("API call failed"));
+        )).thenReturn(responseEntity);
+
+        // when
+        byte[] response = voiceRssService.getVoiceRss("test");
+
+        // then
+        Assertions.assertNull(response);
+    }
+
+    @Test
+    void shouldReturnNullWhenStatusCodeIsNot2xx() {
+        // given
+        byte[] audioData = "audio".getBytes();
+        ResponseEntity<byte[]> responseEntity = ResponseEntity.badRequest().body(audioData);
+
+        when(restTemplate.getForEntity(
+                apiUrl,
+                byte[].class
+        )).thenReturn(responseEntity);
 
         // when
         byte[] response = voiceRssService.getVoiceRss("test");
