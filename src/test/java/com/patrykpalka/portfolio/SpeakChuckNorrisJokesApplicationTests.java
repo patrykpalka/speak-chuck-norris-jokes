67676923444a815531ae306565720e8e74aaffd2
patrykpalka <patryk-palka2@wp.pl>
2025-01-18 15:31:50 +0100

    Test: Add test for main method

--- src/test/java/com/patrykpalka/portfolio/SpeakChuckNorrisJokesApplicationTests.java
+++ src/test/java/com/patrykpalka/portfolio/SpeakChuckNorrisJokesApplicationTests.java
@@ -1,13 +1,36 @@
 package com.patrykpalka.portfolio;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpeakChuckNorrisJokesApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void shouldLogInitializationMessageWhenApplicationStarts() {
+        // given
+        Logger logger = (Logger) LoggerFactory.getLogger(SpeakChuckNorrisJokesApplication.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
 
+        // when
+        SpeakChuckNorrisJokesApplication.main(new String[] {});
+
+        // then
+        assertThat(listAppender.list)
+                .extracting(ILoggingEvent::getMessage)
+                .contains("Initializing Speak Chuck Norris Jokes Application");
+
+        assertThat(listAppender.list)
+                .extracting(ILoggingEvent::getLevel)
+                .contains(Level.INFO);
+    }
 }
