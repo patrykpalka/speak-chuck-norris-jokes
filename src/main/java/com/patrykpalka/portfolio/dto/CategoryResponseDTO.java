e2c6a8c241f895ad04c02967bbdb6248398a2666
patrykpalka <patryk-palka2@wp.pl>
2025-01-20 17:47:01 +0100

    Feat: Add documentation to DTOs

--- src/main/java/com/patrykpalka/portfolio/dto/CategoryResponseDTO.java
+++ src/main/java/com/patrykpalka/portfolio/dto/CategoryResponseDTO.java
@@ -1,8 +1,12 @@
 package com.patrykpalka.portfolio.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
+@Schema(description = "Response containing available joke categories")
 public record CategoryResponseDTO(
+        @Schema(description = "List of available joke categories", example = "[\"animal\", \"career\", \"celebrity\"]")
         List<String> categories
 ) {
 }
--- src/main/java/com/patrykpalka/portfolio/dto/ErrorResponseDTO.java
+++ src/main/java/com/patrykpalka/portfolio/dto/ErrorResponseDTO.java
@@ -1,8 +1,14 @@
 package com.patrykpalka.portfolio.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Response containing error message, timestamp and path")
 public record ErrorResponseDTO(
+        @Schema(description = "Error message", example = "Category is not valid")
         String message,
+        @Schema(description = "Timestamp of the error", example = "2025-01-20T17:44:46.610191700")
         String timestamp,
+        @Schema(description = "Path of the request", example = "/jokes/random")
         String path
 ) {
 }
